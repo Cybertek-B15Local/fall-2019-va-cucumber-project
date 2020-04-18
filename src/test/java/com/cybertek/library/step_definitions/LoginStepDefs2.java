5779e420c03c11f945a586ddd4d9b21d01ec0e12
marufjont <marufjont@gmail.com>
2020-04-18 14:41:56 -0400

    login 2

--- src/test/java/com/cybertek/library/step_definitions/LoginStepDefs2.java
+++ src/test/java/com/cybertek/library/step_definitions/LoginStepDefs2.java
@@ -1,4 +1,23 @@
 package com.cybertek.library.step_definitions;
 
+import io.cucumber.java.en.When;
+
 public class LoginStepDefs2 {
+
+    @When("I enter username {string}")
+    public void i_enter_username(String username) {
+        System.out.println(username);
+    }
+
+    @When("I enter password {string}")
+    public void i_enter_password(String string) {
+        System.out.println(string);
+    }
+
+    @When("click the sign in button")
+    public void click_the_sign_in_button() {
+        System.out.println("Clicking on sign in");
+    }
+
+
 }
