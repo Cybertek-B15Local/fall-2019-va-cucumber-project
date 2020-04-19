75ff05eff23eeceb8b92e1cd1d050197dcba6df4
marufjont <marufjont@gmail.com>
2020-04-19 10:43:23 -0400

    added BrowserUtils.java

--- src/test/java/com/cybertek/library/pages/PageBase.java
+++ src/test/java/com/cybertek/library/pages/PageBase.java
@@ -1,10 +1,24 @@
 package com.cybertek.library.pages;
 
 import com.cybertek.library.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class PageBase {
     public PageBase() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//span[@class='title'][.='Users']")
+    public WebElement users;
+
+    @FindBy(xpath = "//span[@class='title'][.='Dashboard']")
+    public WebElement dashboard;
+
+    @FindBy(xpath = "//span[@class='title'][.='Books']")
+    public WebElement books;
+
+    @FindBy(tagName = "h3")
+    public WebElement pageHeader;
 }
