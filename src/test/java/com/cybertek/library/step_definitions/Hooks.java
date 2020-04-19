6bfad038086c70ad4e00059331214004dbeb8172
marufjont <marufjont@gmail.com>
2020-04-19 12:23:26 -0400

    added hooks

--- src/test/java/com/cybertek/library/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/library/step_definitions/Hooks.java
@@ -3,20 +3,33 @@ package com.cybertek.library.step_definitions;
 
 import com.cybertek.library.utilities.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
     @Before
-    public void setUpScenario(){
+    public void setUpScenario() {
         Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().fullscreen();
     }
 
     @After
-    public void tearDownScenario(){
+    public void tearDownScenario() {
         Driver.closeDriver();
     }
+
+    @BeforeStep
+    public void setUpStep() {
+        System.out.println("prints before every step");
+    }
+
+    @AfterStep
+    public void step(){
+        System.out.println("prints after every step");
+    }
+
 }
