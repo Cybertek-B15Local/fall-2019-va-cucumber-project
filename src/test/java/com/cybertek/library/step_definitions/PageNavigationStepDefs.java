fd86bbabff92432da43233f106311e979c65e0b3
marufjont <marufjont@gmail.com>
2020-04-26 12:20:53 -0400

    my constants

--- src/test/java/com/cybertek/library/step_definitions/PageNavigationStepDefs.java
+++ src/test/java/com/cybertek/library/step_definitions/PageNavigationStepDefs.java
@@ -4,6 +4,7 @@ import com.cybertek.library.pages.DashBoardPage;
 import com.cybertek.library.pages.UsersPage;
 import com.cybertek.library.utilities.BrowserUtils;
 import com.cybertek.library.utilities.Driver;
+import com.cybertek.library.utilities.LibraryConstants;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -79,6 +80,22 @@ public class PageNavigationStepDefs {
         int actualCount = usersPage.allRows.size();
         Assert.assertEquals(expectedCount, actualCount);
 
+    }
+
+    @When("I go/navigate to {string} page")
+    public void i_go_to_page(String page) {
+        System.out.println("Going to page " + page);
+        switch (page.toLowerCase()) {
+            case LibraryConstants.DASHBOARD:
+                dashBoardPage.dashboard.click();
+                break;
+            case LibraryConstants.USERS:
+                dashBoardPage.users.click();
+                break;
+            case LibraryConstants.BOOKS:
+                dashBoardPage.books.click();
+                break;
+        }
     }
 
 
--- src/test/java/com/cybertek/library/utilities/LibraryConstants.java
+++ src/test/java/com/cybertek/library/utilities/LibraryConstants.java
@@ -3,4 +3,7 @@ package com.cybertek.library.utilities;
 public class LibraryConstants {
     public static final String STUDENT = "student";
     public static final String LIBRARIAN = "librarian";
+    public static final String DASHBOARD = "dashboard";
+    public static final String USERS = "users";
+    public static final String BOOKS = "books";
 }
