58b624f279c261ef86b762a12d10ed3d177ad3e1
marufjont <marufjont@gmail.com>
2020-04-26 14:31:12 -0400

    Added a book pojo, update page object

--- src/test/java/com/cybertek/library/step_definitions/BooksTableStepDefinitions.java
+++ src/test/java/com/cybertek/library/step_definitions/BooksTableStepDefinitions.java
@@ -3,6 +3,7 @@ package com.cybertek.library.step_definitions;
 import com.cybertek.library.pages.BooksPage;
 import com.cybertek.library.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import java.util.List;
 
@@ -10,17 +11,28 @@ import static org.junit.Assert.assertTrue;
 
 public class BooksTableStepDefinitions {
     BooksPage booksPage = new BooksPage();
-    @Then("books table should contain results matching {word}")
+    @Then("books table should contain results matching {}")
     public void books_table_should_contain_results_matching(String book) {
         List<String> actualRows = BrowserUtils.getElementsText(booksPage.allRows);
 
         boolean found = true;
         for (String row : actualRows) {
-            if (!row.contains(book)) {
+            if (!row.toLowerCase().contains(book.toLowerCase())) {
                 found = false;
             }
         }
 
         assertTrue(book + " was not found in books table", found);
     }
+
+
+
+    @When("I edit book {}")
+    public void i_edit_book_The_kiterunner(String book) {
+        System.out.println("book = " + book);
+        BrowserUtils.waitForClickability(booksPage.search, 5).sendKeys(book);
+        BrowserUtils.waitForClickability(booksPage.editBook(book), 5).click();
+
+    }
+
 }
--- src/test/resources/features/BooksTable.feature
+++ src/test/resources/features/BooksTable.feature
@@ -1,10 +1,16 @@
 Feature: books table
 
-  @wip
+
   Scenario: Verify search results
     Given I am on the login page
     And I login to application as a student
     When I navigate to "Books" page
-    And I search for "1984"
-    Then books table should contain results matching 1984
+    And I search for "The Goldfinch"
+    Then books table should contain results matching The Goldfinch
 
+  @wip
+  Scenario: Verify book information
+    Given I am on the login page
+    And I login to application as a librarian
+    And I navigate to "Books" page
+    When I edit book The kite runner
