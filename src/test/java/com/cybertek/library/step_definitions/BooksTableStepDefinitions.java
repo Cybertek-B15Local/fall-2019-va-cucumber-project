2687bfd140391986831f92f0c7112e88016a83d5
marufjont <marufjont@gmail.com>
2020-04-26 15:11:02 -0400

    Added a book pojo, update page object

--- src/test/java/com/cybertek/library/step_definitions/BooksTableStepDefinitions.java
+++ src/test/java/com/cybertek/library/step_definitions/BooksTableStepDefinitions.java
@@ -1,11 +1,14 @@
 package com.cybertek.library.step_definitions;
 
 import com.cybertek.library.pages.BooksPage;
+import com.cybertek.library.pojos.Book;
 import com.cybertek.library.utilities.BrowserUtils;
+import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
 
@@ -34,5 +37,23 @@ public class BooksTableStepDefinitions {
         BrowserUtils.waitForClickability(booksPage.editBook(book), 5).click();
 
     }
+
+    // register a data table type that uses a custom class
+    @DataTableType
+    public Book convertBook(Map<String, String> dataTable){
+        Book book = new Book(dataTable.get("name"),
+                            dataTable.get("author"),
+                            dataTable.get("year"));
+        return book;
+    }
+
+    @Then("I verify book information")
+    public void i_verify_book_information(Book book) {
+        System.out.println(book);
+        System.out.println("book.getName() = " + book.getName());
+        System.out.println("book.getAuthor() = " + book.getAuthor());
+        System.out.println("book.getYear() = " + book.getYear());
+    }
+
 
 }
--- src/test/resources/features/BooksTable.feature
+++ src/test/resources/features/BooksTable.feature
@@ -14,3 +14,6 @@ Feature: books table
     And I login to application as a librarian
     And I navigate to "Books" page
     When I edit book The kite runner
+    Then I verify book information
+      | name            | author          | year |
+      | The kite runner | Khaled Hosseini | 2003 |
