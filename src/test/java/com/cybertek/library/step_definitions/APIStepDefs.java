af25b44be05351c159aa5f39f8b116b222b9895a
marufjont <marufjont@gmail.com>
2020-06-02 19:55:33 -0400

    api utils

--- src/test/java/com/cybertek/library/step_definitions/APIStepDefs.java
+++ src/test/java/com/cybertek/library/step_definitions/APIStepDefs.java
@@ -1,14 +1,18 @@
 package com.cybertek.library.step_definitions;
 
+import com.cybertek.library.pages.LoginPage;
 import com.cybertek.library.utilities.AuthenticationUtility;
 import com.cybertek.library.utilities.LibraryUserUtility;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 
 import java.util.Map;
 
-public class APIStepDefs {
+import static io.restassured.RestAssured.given;
 
+public class APIStepDefs {
+    Map<String, Object> student;
     @Given("new student is added using the add_user endpoint")
     public void new_student_is_added_using_the_add_user_endpoint() {
         // set base url --> let's in Hooks class
@@ -16,14 +20,24 @@ public class APIStepDefs {
         String librarianToken = AuthenticationUtility.getLibrarianToken();
         System.out.println("librarianToken = " + librarianToken);
         // create new user information
-        Map<String, Object> student = LibraryUserUtility.createUser(3);
+        student = LibraryUserUtility.createUser(3);
         System.out.println("student = " + student);
         // create using using the add_user
+        Response response = given().
+                header("x-library-token", librarianToken).
+                formParams(student).
+                log().all().
+        when().
+                post("add_user").
+                prettyPeek();
+        response.then().statusCode(200);
     }
 
     @When("I login as the new user created using add_user endpoint")
     public void i_login_as_the_new_user_created_using_add_user_endpoint() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }// BREAK 7.15
+        String email = student.get("email").toString();
+        String password = student.get("password").toString();
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(email, password);
+    }
 }
