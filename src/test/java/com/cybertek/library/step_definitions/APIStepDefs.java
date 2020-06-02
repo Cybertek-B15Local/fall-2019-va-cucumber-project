33775a0b7eed7e7d827febe056ada7480d654ec2
marufjont <marufjont@gmail.com>
2020-06-02 19:25:54 -0400

    api utils

--- src/test/java/com/cybertek/library/step_definitions/APIStepDefs.java
+++ src/test/java/com/cybertek/library/step_definitions/APIStepDefs.java
@@ -1,8 +1,11 @@
 package com.cybertek.library.step_definitions;
 
 import com.cybertek.library.utilities.AuthenticationUtility;
+import com.cybertek.library.utilities.LibraryUserUtility;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+
+import java.util.Map;
 
 public class APIStepDefs {
 
@@ -13,6 +16,8 @@ public class APIStepDefs {
         String librarianToken = AuthenticationUtility.getLibrarianToken();
         System.out.println("librarianToken = " + librarianToken);
         // create new user information
+        Map<String, Object> student = LibraryUserUtility.createUser(3);
+        System.out.println("student = " + student);
         // create using using the add_user
     }
 
@@ -20,5 +25,5 @@ public class APIStepDefs {
     public void i_login_as_the_new_user_created_using_add_user_endpoint() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
-    }
+    }// BREAK 7.15
 }
